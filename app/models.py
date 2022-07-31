fa81f74d41722f0515f3c8986391de533b46cd34
Jasmin-S1 <101867431+Jasmin-S1@users.noreply.github.com>
2022-07-31 19:01:32 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -2,8 +2,6 @@ from sqlalchemy import TIMESTAMP, Boolean, Column, ForeignKey, Integer, String, 
 from .database import Base
 from sqlalchemy.orm import relationship
 
- 
-
 class Post(Base):
     __tablename__ = "posts"
 
@@ -12,23 +10,21 @@ class Post(Base):
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), server_default=text('NOW()'), nullable=False)
-    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)    # kreiranje nove kolone i postavljanje foreign-key
-                                          # (users.id) - referenca na tabelu i kolonu
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)   
+                                         
                                           
     # set a relationship - say sqlalchemy to get some information about user based on for example user_id
-    user = relationship("User")  # fetch the user based on user_id. Veza je na klasu User
-                                 # potrebno je update-ovati i schema-u
+    user = relationship("User")  # fetch the user based on user_id
+                                
 
 
-class User(Base):                # all sqlalchemy model extend Base model
+class User(Base):                
     __tablename__ = "users"
     
     id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), server_default=text('NOW()'), nullable=False)
-
-
 
 class Vote(Base):
     __tablename__ = "votes"
