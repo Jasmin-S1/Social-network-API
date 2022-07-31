1b2e10bb07e36a6761eeaab973b7414908f9b02b
Jasmin-S1 <101867431+Jasmin-S1@users.noreply.github.com>
2022-07-31 19:07:25 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
-from pydantic import BaseSettings # za validaciju environment varijabli
+from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    database_hostname: str      # praksa je da env. varijable budu velikim slovima ali ce pydantic automatski konvertovati u velika slova
+    database_hostname: str      
     database_port: str
     database_password: str  
     database_name: str 
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     algorithm: str
     access_token_expire_minutes: int
 
-    class Config:         # import env. varijabli iz .env fajla
+    class Config:       
         env_file = ".env"
- 
+        
 settings = Settings()
