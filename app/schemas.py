8f6ec5f71eabb73a5310690c820601035e639c90
Jasmin-S1 <101867431+Jasmin-S1@users.noreply.github.com>
2022-07-31 19:06:17 +0200

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -2,44 +2,28 @@ from typing import Optional
 from pydantic import BaseModel, EmailStr, conint
 from datetime import datetime
 
-
-
-
-
 class Post(BaseModel):      
     title: str              
     content: str           
     published: bool = True  
-
 
 class CreatePost(BaseModel):      
     title: str              
     content: str           
     published: bool = True  
 
-
 class UpdatePost(BaseModel):      
     title: str              
     content: str           
     published: bool
 
-
-
-
-# mozemo imati posebno definisanu klasu za svaki od requesta
-
-# mozemo i na drugaciji nacin definisati klase
-
-class PostBase(BaseModel):         # all pydantic model always extend BaseModel
+class PostBase(BaseModel):         
     title: str              
     content: str           
     published: bool = True  
 
-
-
-class PostCreate(PostBase):    # ovu klasu treba i pozivati kod post i put metoda
+class PostCreate(PostBase):   
     pass
-
 
 class UserResponse(BaseModel):
     id: int
@@ -49,38 +33,25 @@ class UserResponse(BaseModel):
     class Config:
         orm_mode = True
 
-# definisanje pydantic modela za response 
-class PostResponse(PostBase):    # sva ostala polja ce naslijediti iz klase PostBase 
+class PostResponse(PostBase):   
     created_at: datetime      
     user_id: int
-    user: UserResponse   # ovo polje ce sadrzavati informacije o user-u koji je kreirao post (referira se na klasu UserResponse)
+    user: UserResponse   
 
     class Config:
         orm_mode = True
 
-
-#definisanje response (pydantic) modela za join tabela posts i votes
 class PostVotes(PostBase):  
-    Post: PostResponse     # definisano je ovako jer query vraca dva polja, 1. Post i 2. votes, izgled response modela u fajlu example_join_tabela.py
+    Post: PostResponse    
     votes: int
 
-    
-
-
 class UserCreate(BaseModel):
-    email: EmailStr          # Emailstr je email validator - provjerava da li je validan mail a ne neki random tekst
-    password: str            # ne radi validacija email-a, ne moze da importuje email-validator
-
-# kada pohranjujemo password u bazu ne pohranjujemo ga kao plain tekst, vec uradimo hash passworda
-# potrebno je instalirati (pip install passlib[bcrypt])
-
-
-
+    email: EmailStr          
+    password: str           
 
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
-
 
 class Token(BaseModel):
     access_token: str
@@ -89,7 +60,6 @@ class Token(BaseModel):
 class TokenData(BaseModel):
     id: Optional[str] = None
 
-
 class Vote(BaseModel):
     post_id: int
-    direction: conint(le=1)  # samo dvije opcije "les than" or "equal to" 1 (ako se lajkuje ide 1, ako ne onda  0)
+    direction: conint(le=1)  
