0c8962d8776748994bef5754398376f992e45742
Jasmin-S1 <101867431+Jasmin-S1@users.noreply.github.com>
2022-07-31 19:26:14 +0200

    Update user.py

--- app/routers/user.py
+++ app/routers/user.py
@@ -3,26 +3,20 @@ from .. import models, schemas, utils
 from sqlalchemy.orm import Session
 from ..database import get_db
 
-
-# necemo importovati app objekat vec koristiti APIRouter, i @app.post --> @router.post i tako dalje
 router = APIRouter(
     prefix="/users",
     tags=['Users']
 )
 
-
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserResponse)  
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-
     hashed_password = utils.hash(user.password) 
     user.password = hashed_password
-
     new_user = models.User(**user.dict())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
-
 
 @router.get("/{id}", response_model=schemas.UserResponse)
 def get_user(id: int, db: Session = Depends(get_db)):
@@ -32,5 +26,3 @@ def get_user(id: int, db: Session = Depends(get_db)):
     return user
 
 
-# sva logika oko hash-iranja passworda se moze premjestiti u "util.py". Util file je zbirka malih funkcija i klasa
-# kreiranje route-a za vracanje informacija o pojedinacnom user-u. Npr kod twitera za vracanje informacija o profilu
