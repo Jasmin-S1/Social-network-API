017b0870e23096df22f89a82afc8f3cbbd2172f9
Jasmin-S1 <101867431+Jasmin-S1@users.noreply.github.com>
2022-07-31 19:04:35 +0200

    Update oauth2.py

--- app/oauth2.py
+++ app/oauth2.py
@@ -6,50 +6,33 @@ from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from .config import settings
 
-oauth2_schema = OAuth2PasswordBearer(tokenUrl="login")                      # ovo ce biti endpoint login-a
+oauth2_schema = OAuth2PasswordBearer(tokenUrl="login")                      
 
-# Moramo obezbijediti sljedece podatke:
-# 1. SECRET_KEY
-# 2. Algorithm
-# 3. Expriation time
-
-SECRET_KEY = settings.secret_key # ova varijabla i ove ispod se ne pisu u kodu vec se kreiraju environment variable
+SECRET_KEY = settings.secret_key 
 ALGORITHM = settings.algorithm
 ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
 def create_access_token(data: dict):
-    to_encode = data.copy() # radi sigurnosti kreiramo kopiju podataka i pohranimo ih u neku varijablu
-
+    to_encode = data.copy() 
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp": expire})  # dodaje novi "key" u dictionary "data"
-
-    encodeed_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM) # pozivanje funkcije koja kreira token
-    # to_encode - su podaci za payload, ostalo je definisano
-
+    to_encode.update({"exp": expire}) 
+    encodeed_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encodeed_jwt
 
-
 def verify_access_token(token: str, credential_exception):
-    
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])   # algoritam ide u srednju zagradu zato sto moze biti lista algoritama
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])  
         id: str = payload.get("user_id") 
-
         if id is None:
             raise credential_exception
         token_data = schemas.TokenData(id= id)
     except JWTError:
         raise credential_exception
-    
     return token_data
-
 
 def get_current_user(token: str = Depends(oauth2_schema), db: Session = Depends(database.get_db)):
     credential_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
                                         detail=f"Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
-    
     token = verify_access_token(token, credential_exception)
-
     user = db.query(models.User).filter(models.User.id == token.id).first()
-
     return user
