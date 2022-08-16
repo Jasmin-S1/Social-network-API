41fe3abddeafde8f4431f91173b0fce53801f750
Jasmin-S1 <101867431+Jasmin-S1@users.noreply.github.com>
2022-08-16 16:42:52 +0200

    Update vote.py

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -11,10 +11,10 @@ router = APIRouter(
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def vote(vote: schemas.Vote, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
 
-    vote_query = db.query(models.Vote).filter(models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user.id) # provjera dva uslova
+    vote_query = db.query(models.Vote).filter(models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user.id) 
     found_vote = vote_query.first()
     if(vote.direction == 1):
-        if found_vote: # ako vec postoji vote onda ide HTTPException, ako nema onda se vote dodaje u bazu
+        if found_vote: 
             raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f'User {current_user.id} has already voted on post {vote.post_id}')
         new_vote = models.Vote(post_id = vote.post_id, user_id = current_user.id) 
         db.add(new_vote)
